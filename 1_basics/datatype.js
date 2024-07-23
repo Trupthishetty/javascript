741ea964f16f637f8c134d516fe7594a0d02989b
Trupthishetty <71178707+Trupthishetty@users.noreply.github.com>
2024-07-23 16:42:56 +0000

    datatypes

---
+++ 1_basics/datatype.js
@@ -0,0 +1,25 @@
+"use strict" ; //treat all js as newer js
+//alert(3+3); //throw error because we are using nodejs and not browser
+console.log(3+3);
+console.log("Sam") //code readability "tc39.es/ecma262" or "mdn"
+
+let name ="Sam "
+let age=18
+let loggedIn=false
+
+//number. 2 pwr 53
+//bigint. larger numbers
+//string " "
+//boolean true/false
+//null standalone value
+//undefined 
+//symbol to define uniqueness
+
+
+//object
+
+console.log(typeof(age));
+console.log(typeof "trupthi");
+
+console.log(typeof null);//datattype is an object 
+console.log(typeof undefined )///datatype is undefined 
---
+++ 1_basics/var.js
@@ -0,0 +1,16 @@
+const accountId="1234"
+let accountEmail = "trupthi@gamil.com"
+var accountPassword ="12345"
+accountCity="Jaipur"
+let accountState;
+
+//accountId="234"
+
+
+accountEmail="eee@gmail.com"
+accountPassword= "890"
+accountCity="banglore"
+
+console.log(accountId);
+
+console.table([accountId ,accountEmail, accountPassword, accountCity,accountState]);
