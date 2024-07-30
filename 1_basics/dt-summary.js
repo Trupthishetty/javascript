e737ab6560e41353837c9e8ccfeb90549458b389
Trupthishetty <71178707+Trupthishetty@users.noreply.github.com>
2024-07-30 15:41:30 +0000

    summary

---
+++ 1_basics/dt-summary.js
@@ -0,0 +1,33 @@
+//depending on how the data is stored and how it is accessed there are 2 types:primitive nad non primitive .
+
+
+//primitive:string, number , boolean,null , undefined ,symbol
+const score= 100
+const sc=10.4
+
+const a=false
+const b=null
+
+const c= Symbol('123')
+const d=c
+
+console.log(c===d)
+const bifNumber=12345678
+//non primitice (reference) : array , objects, functions  and return type is function .
+
+
+
+const ar=["1",'2','4']
+
+let x={
+    name:"sam",
+    age:"34"
+}
+
+// const fun= func()
+// {
+//     console.log("hellow");
+// }
+
+
+console.log(typeof(c));
